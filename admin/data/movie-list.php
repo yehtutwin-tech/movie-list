d7e38e5d9c3043b7e66f18e81f90ecc3f7f2276b
Ye Htut Win <yehtutwin.tech@gmail.com>
2024-08-28 10:39:37 +0630

    temporary data store in separate file

---
+++ admin/data/movie-list.php
@@ -0,0 +1,27 @@
+<?php
+$data = [
+  [
+    "id" => 1,
+    "title" => "Jackie Chan Adventures",
+    "year" => "2000–2005",
+    "type" => "series",
+    "poster" => "./images/jc1.jpg",
+    "created_at" => "2024-08-28 05:43AM"
+  ],
+  [
+    "id" => 2,
+    "title" => "Jackie Chan: My Stunts",
+    "year" => "1999",
+    "type" => "movie",
+    "poster" => "./images/jc2.jpg",
+    "created_at" => "2024-08-28 05:43AM"
+  ],
+  [
+    "id" => 3,
+    "title" => "Jackie Chan Presents: Wushu",
+    "year" => "2008",
+    "type" => "movie",
+    "poster" => "./images/jc3.jpg",
+    "created_at" => "2024-08-28 05:43AM"
+  ],
+];
