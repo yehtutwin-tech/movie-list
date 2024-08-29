ce8302cc2d807b020b4f5f732e29c18e10aa456e
Ye Htut Win <yehtutwin.tech@gmail.com>
2024-08-29 11:05:24 +0630

    bootstrap, fontawesome - library files added

--- admin/partial/header.php
+++ admin/partial/header.php
@@ -11,7 +11,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Movie List</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="<?= PROJECT_ROOT ?>/assets/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= PROJECT_ROOT ?>/assets/fontawesome/css/all.min.css" rel="stylesheet">
   </head>
   <body>
     <div class="container">
--- admin/type/index.php
+++ admin/type/index.php
@@ -14,18 +14,14 @@
   if (!$result) {
     die('query failed: ' . $conn->error);
   }
-
-  // while($row = $result->fetch_assoc()) {
-  //   echo $row['name'];
-  //   echo "<br />";
-  // }
 ?>
 <hr/>
-<table border="1">
+<table class="table table-bordered table-strped table-hover">
   <thead>
     <tr>
       <th>Name</th>
       <th>Created At</th>
+      <th>Actions</th>
     </tr>
   </thead>
   <tbody>
@@ -33,6 +29,20 @@
     <tr>
       <td><?= $row['name'] ?></td>
       <td><?= $row['created_at'] ?></td>
+      <td>
+        <a
+          href="<?= PROJECT_ROOT ?>/admin/type/edit.php?id=<?= $row['id'] ?>"
+          class="btn btn-warning"
+        >
+          <i class="fa-regular fa-pen-to-square"></i>
+        </a>
+        <a
+          href="<?= PROJECT_ROOT ?>/admin/type/delete.php?id=<?= $row['id'] ?>"
+          class="btn btn-danger"
+        >
+          <i class="fa-solid fa-trash-can"></i>
+        </a>
+      </td>
     </tr>
     <?php } ?>
   </tbody>
